dfc6d36e750abc6f3941e47c66dd1e8b8392d1f0
gauravghosh24 <153831494+gauravghosh24@users.noreply.github.com>
2025-05-25 15:01:32 +0000

    operations

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -8,7 +8,7 @@ console.log(typeof valueInNumber);
 
 //in javascript we should be carefull of 
 
-let s1="33abc"
+let s1="33abc"           
 
 console.log(typeof(s1));
 
@@ -27,3 +27,29 @@ let q="";
 console.log(Boolean(q));
 let q1="gaurav";
 console.log(Boolean(q1));
+
+//////////////operation/////////\
+console.log(1+2+'3');
+console.log("1"+2+2);
+//what come first it is converted to that 
+console.log(+true);
+console.log(+"");
+
+//prefix and postfix
+let a=10;
+let b=a++;
+let c=++a;
+console.log(b);//postfix first it assign the value then increment 
+console.log(c);//prefix it increment the value the assign it to the variable 
+
+//comoparisons
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);//confusing so plz avoid 
+
+
+//the reason is that an equality check == and compariosn >,<,>=,<= work differntlt
+//compariosn convert null to a number ,treating it as 0
+//That is why null >=0 is true and null>0 is false
+
+//=== strict check also check the datatype
