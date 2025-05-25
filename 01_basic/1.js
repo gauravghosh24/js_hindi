19b6d67e375c83a1ee6d100c58a3dc713fff8233
gauravghosh24 <153831494+gauravghosh24@users.noreply.github.com>
2025-05-25 13:26:04 +0000

    datatype

---
+++ 01_basic/1.js
@@ -0,0 +1,8 @@
+const accountId=144553//can not be changed
+let accountEmail="gaurav@gamil.com"//USED
+var accountPassword="12345"//NOT used generallly because of use in functional 
+accountCity='Kolkata'
+let accountState;// is the value is not assigned in a variable then it is set as undefined 
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basic/2.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+//number 
+//bigint
+//string
+//boolean
+//null=>standalone (empty)//(object type)
+//undefined=>no value is given(undefined only)
+
+//object 
+console.log(typeof(null));//this is a interview question
+console.log(typeof(undefined));
